42aba2e52716019bb4617deaffbaa2fb2cfd9df3
Daniel Barath <majti89@gmail.com>
2019-06-30 20:12:07 -0700

    The estimator solving the minimal case for homography estimation is replaced. In case of having a minimal sample, linear equation system A x = b is given (H_33 = 1 is fixed), where A is of size 8*8. Therefore, the SVD-based solution can be replaced by a simple inversion.

--- src/pymagsac/src/magsac_python.cpp
+++ src/pymagsac/src/magsac_python.cpp
@@ -21,7 +21,7 @@ int findFundamentalMatrix_(std::vector<double>& srcPts,
     MAGSAC<FundamentalMatrixEstimator, FundamentalMatrix> magsac;
     magsac.setSigmaMax(sigma_max); // The maximum noise scale sigma allowed
     magsac.setInterruptingThreshold(sigma_th / 3.0f); // The threshold used for speeding up the procedure
-    magsac.setCoreNumber(4); // The number of cores used to speed up sigma-consensus
+    magsac.setCoreNumber(partition_num); // The number of cores used to speed up sigma-consensus
     magsac.setPartitionNumber(partition_num); // The number partitions used for speeding up sigma consensus. As the value grows, the algorithm become slower and, usually, more accurate.
     magsac.setIterationLimit(max_iters);
     magsac.setTerminationCriterion(MAGSAC<FundamentalMatrixEstimator, FundamentalMatrix>::TerminationCriterion::RansacCriterion,
@@ -91,7 +91,7 @@ int findHomography_(std::vector<double>& srcPts,
     MAGSAC<RobustHomographyEstimator, Homography> magsac;
     magsac.setSigmaMax(sigma_max); // The maximum noise scale sigma allowed
     magsac.setInterruptingThreshold(sigma_th / 3.0f); // The threshold used for speeding up the procedure
-    magsac.setCoreNumber(4); // The number of cores used to speed up sigma-consensus
+    magsac.setCoreNumber(partition_num); // The number of cores used to speed up sigma-consensus
     magsac.setPartitionNumber(partition_num); // The number partitions used for speeding up sigma consensus. As the value grows, the algorithm become slower and, usually, more accurate.
     magsac.setIterationLimit(max_iters);
     magsac.setTerminationCriterion(MAGSAC<RobustHomographyEstimator, Homography>::TerminationCriterion::RansacCriterion,
