98615abb2960a539bd0aa9ac1a3d166596dec7d7
ZhangJiahui <zhangjiahui@JiahuideMacBook-Pro.local>
2020-06-13 17:38:56 +0800

    add essential

--- src/pymagsac/src/magsac_python.cpp
+++ src/pymagsac/src/magsac_python.cpp
@@ -76,6 +76,78 @@ int findFundamentalMatrix_(std::vector<double>& srcPts,
     return num_inliers;
 }
 
+int findEssentialMatrix_(std::vector<double>& srcPts,
+                           std::vector<double>& dstPts,
+                           std::vector<bool>& inliers,
+                           std::vector<double>& E,
+                           std::vector<double>& intrinsics_src;
+                           std::vector<double>& intrinsics_dst;
+                           double sigma_max,
+                           double conf,
+                           int max_iters,
+                           int partition_num,
+                           int core_num = 1)
+{
+    
+    magsac::utils::DefaultEssentialMatrixEstimator estimator(Eigen::Map<Eigen::Matrix3d>(intrinsics_src.data()),
+                                                            Eigen::Map<Eigen::Matrix3d>(intrinsics_dst.data()),
+                                                            0.1); // The robust homography estimator class containing the
+    gcransac::EssentialMatrix model; // The estimated model
+    
+    MAGSAC<cv::Mat, magsac::utils::DefaultEssentialMatrixEstimator> magsac;
+    magsac.setMaximumThreshold(sigma_max); // The maximum noise scale sigma allowed
+    //magsac.setInterruptingThreshold(sigma_th / 3.0f); // The threshold used for speeding up the procedure
+    magsac.setCoreNumber(core_num); // The number of cores used to speed up sigma-consensus
+    magsac.setPartitionNumber(partition_num); // The number partitions used for speeding up sigma consensus. As the value grows, the algorithm become slower and, usually, more accurate.
+    magsac.setIterationLimit(max_iters);
+    //magsac.setTerminationCriterion(MAGSAC<FundamentalMatrixEstimator, FundamentalMatrix>::TerminationCriterion::RansacCriterion,
+    //    sigma_th); // Use the standard RANSAC termination criterion since the MAGSAC one is too pessimistic and, thus, runs too long sometimes
+
+    int num_tents = srcPts.size()/2;
+    cv::Mat points(num_tents, 4, CV_64F);
+    for (int i = 0; i < num_tents; ++i) {
+        points.at<double>(i, 0) = srcPts[2*i];
+        points.at<double>(i, 1) = srcPts[2*i + 1];
+        points.at<double>(i, 2) = dstPts[2*i];
+        points.at<double>(i, 3) = dstPts[2*i + 1];
+    }
+    gcransac::sampler::UniformSampler main_sampler(&points);
+
+    bool success =  magsac.run(points, // The data points
+                              conf, // The required confidence in the results
+                              estimator, // The used estimator
+                              main_sampler, // The sampler used for selecting minimal samples in each iteration
+                              model, // The estimated model
+                              max_iters); // The number of iterations
+    inliers.resize(num_tents);
+    if (!success) {
+        for (auto pt_idx = 0; pt_idx < points.rows; ++pt_idx) {
+            inliers[pt_idx] = false;
+        }
+        E.resize(9);
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                E[i*3+j] = 0;
+            }
+        }
+        return 0;
+    }
+    int num_inliers = 0;
+    for (auto pt_idx = 0; pt_idx < points.rows; ++pt_idx) {
+        const int is_inlier = estimator.residual(points.row(pt_idx), model.descriptor) <= sigma_max;
+        inliers[pt_idx] = (bool)is_inlier;
+        num_inliers+=is_inlier;
+    }
+    
+    E.resize(9);
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            E[i*3+j] = (double)model.descriptor(i,j);
+        }
+    }
+    return num_inliers;
+}
+
 
 int findHomography_(std::vector<double>& srcPts,
                     std::vector<double>& dstPts,
