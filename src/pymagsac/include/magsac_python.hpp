093d2adb9a88c134c54a285b7af09080eb2a637f
zjhthu <jiahui_z11@163.com>
2020-06-15 17:06:22 +0800

    run essential

--- src/pymagsac/include/magsac_python.hpp
+++ src/pymagsac/include/magsac_python.hpp
@@ -13,6 +13,17 @@ int findFundamentalMatrix_(std::vector<double>& srcPts,
                            int max_iters = 10000,
                            int partition_num = 5);
 
+int findEssentialMatrix_(std::vector<double>& srcPts,
+                           std::vector<double>& dstPts,
+                           std::vector<bool>& inliers,
+                           std::vector<double>&  E,
+                           std::vector<double>& intrinsics_src,
+                           std::vector<double>& intrinsics_dst,
+                           double sigma_th = 3.0,
+                           double conf = 0.99,
+                           int max_iters = 10000,
+                           int partition_num = 5,
+                           int core_num = 1);
 
                 
 int findHomography_(std::vector<double>& srcPts,
